39d76acdb2d09e4a95acac7c97134d024be2c44d
Martine Gagne <martine.e.gagne@gmail.com>
2017-06-29 17:17:18 -0400

    completed_exercise_6.1_pt_3

---
+++ exercise_6.1.rb
@@ -0,0 +1,21 @@
+#Exercise_6.1_pt_1
+movie_year = {
+  1991 => ["Beauty and the Beast", "Silence of the Lambs", "Terminator 2"],
+  1997 => ["Titanic", "Men in Black", "Boogie Nights"],
+  2004 => ["The Aviator", "Eternal Sunshine", "Kill Bill Volume 2"]
+}
+
+#Exercise_6.1_pt_2
+row_1 = [1, 2, 3]
+row_2 = [4, 5, 6]
+row_3 = [7, 8, 9]
+row_4 = ["*",0,"#"]
+
+phone_buttons = [[1, 2, 3], [4, 5, 6], [7, 8, 9], ["*", 0, "#"]]
+
+#exercise_6.1_part_3
+countries = [
+  {country = > "Scotland", "Europe", "Not an Island"},
+  {country => "Findland", "Europe", "Not an Island"},
+  {country => "Cuba", "North America", "island" }
+]
