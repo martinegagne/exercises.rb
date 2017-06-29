8df91fb3308d9e567d4b108f174adf93020dd387
Martine Gagne <martine.e.gagne@gmail.com>
2017-06-29 17:26:38 -0400

    completed_exercise_6.2_pt_3

--- exercise_6.2.rb
+++ exercise_6.2.rb
@@ -1,4 +1,18 @@
 #exercise_6.2_pt_1
-20.times do 
+20.times do
   puts "I will not skateboard in the halls."
 end
+
+#exercise_6.2_pt_2
+message = ["I will not skateboard in the halls.", "I will not skateboard in the halls.",
+  "I will not skateboard in the halls.", "I will not skateboard in the halls.",
+  "I will not skateboard in the halls.", "I will not skateboard in the halls.",
+  "I will not skateboard in the halls.", "I will not skateboard in the halls.",
+  "I will not skateboard in the halls.", "I will not skateboard in the halls.",
+  "I will not skateboard in the halls.", "I will not skateboard in the halls.",
+  "I will not skateboard in the halls.", "I will not skateboard in the halls.",
+  "I will not skateboard in the halls.", "I will not skateboard in the halls.",
+  "I will not skateboard in the halls.", "I will not skateboard in the halls.",
+  "I will not skateboard in the halls.", "I will not skateboard in the halls."]
+
+#exercise_6.2_pt_3
