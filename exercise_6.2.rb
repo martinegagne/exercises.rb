63e57690c46317b67e9f543dc9fde19bdfb73218
Martine Gagne <martine.e.gagne@gmail.com>
2017-06-29 18:48:59 -0400

    completed_exercise_6.2

--- exercise_6.2.rb
+++ exercise_6.2.rb
@@ -4,15 +4,30 @@
 end
 
 #exercise_6.2_pt_2
-message = ["I will not skateboard in the halls.", "I will not skateboard in the halls.",
-  "I will not skateboard in the halls.", "I will not skateboard in the halls.",
-  "I will not skateboard in the halls.", "I will not skateboard in the halls.",
-  "I will not skateboard in the halls.", "I will not skateboard in the halls.",
-  "I will not skateboard in the halls.", "I will not skateboard in the halls.",
-  "I will not skateboard in the halls.", "I will not skateboard in the halls.",
-  "I will not skateboard in the halls.", "I will not skateboard in the halls.",
-  "I will not skateboard in the halls.", "I will not skateboard in the halls.",
-  "I will not skateboard in the halls.", "I will not skateboard in the halls.",
-  "I will not skateboard in the halls.", "I will not skateboard in the halls."]
+# message = ["I will not skateboard in the halls.", "I will not skateboard in the halls.",
+#   "I will not skateboard in the halls.", "I will not skateboard in the halls.",
+#   "I will not skateboard in the halls.", "I will not skateboard in the halls.",
+#   "I will not skateboard in the halls.", "I will not skateboard in the halls.",
+#   "I will not skateboard in the halls.", "I will not skateboard in the halls.",
+#   "I will not skateboard in the halls.", "I will not skateboard in the halls.",
+#   "I will not skateboard in the halls.", "I will not skateboard in the halls.",
+#   "I will not skateboard in the halls.", "I will not skateboard in the halls.",
+#   "I will not skateboard in the halls.", "I will not skateboard in the halls.",
+#   "I will not skateboard in the halls.", "I will not skateboard in the halls."]
+
+
+message = ["I will not skateboard in the halls."]
+  message.cycle(20)
 
 #exercise_6.2_pt_3
+numbers_count = (1..50).to_a
+
+#exercise_6.2_pt_4
+p numbers_count.each.sum
+
+#exercise_6.3_pt_5
+numbers_count = (1..50).to_a
+
+p numbers_count.cycle(3) {|x| puts x}
+
+#exercise_6.3_pt_6
